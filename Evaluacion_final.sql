d4d047ac34e95a8df6d0d13246d15679531cb9f3
saira2911 <anasaira29@gmail.com>
2024-03-11 10:37:40 +0100

    add up to ex 15

--- Evaluacion_final.sql
+++ Evaluacion_final.sql
@@ -12,7 +12,7 @@ WHERE `rating` = 'PG-13';
 -- 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
 SELECT `title`, `description`
 	FROM `film`
-WHERE `description` LIKE '%amazing%';
+WHERE `description` LIKE '%amazing%'; 
 
 -- 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
 SELECT `title`
@@ -44,21 +44,58 @@ SELECT COUNT(`film_id`) AS `movies number`, `rating`
 GROUP BY `rating`;
 
 -- 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.
-SELECT COUNT(`inventory_id`) AS `rented movies`, `customer_id`, `first_name`, `last_name`
+SELECT COUNT(`inventory_id`) AS `rented movies`, `customer_id`, `first_name`, `last_name` -- cada id_inventario se refiere a una pelicula, por lo que sabiendo en num de inventarios relacionados con el cliente sabemos el num de peliculas que ha alquilado
 	FROM `customer`
 LEFT JOIN `rental`
 	USING (`customer_id`)
-GROUP BY `customer_id`
+GROUP BY `customer_id`; -- agrupamos por cliente
 
 -- 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres.
 
+-- Category -> film_category -> inventory -> rental. 
+-- Hacemos joins entre tablas hasta unir las que contienen los datos que nos solicitan, hacemos LEFT JOIN por si existe 
+-- un genero del que no se haya alquilado ninguna pelicula. Contamos inventory_id ya que correcponde al total de peliculas  
+-- alquiladas incluyendo duplicados (misma pelicula alquilada varias veces) y agrupamos por los nombres de las categorias.
+SELECT COUNT(`r`.`inventory_id`) AS `rented movies`, `c`.`name` AS `category`
+	FROM `category` AS `c`
+LEFT JOIN `film_category` AS `fc`
+	USING (category_id)
+LEFT JOIN `inventory` AS `i`
+	USING (`film_id`)
+LEFT JOIN `rental` AS `r`
+	USING (inventory_id)
+GROUP BY `c`.`name`;
+
 -- 12. Encuentra el promedio de duración de las películas para cada clasificación de la tabla `film` y muestra la clasificación junto con el promedio de duración.
+SELECT AVG(`length`) AS `average length`, `rating`
+	FROM `film`
+GROUP BY `rating`;
 
 -- 13. Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
 
+-- actor -> film_actor -> film 
+-- hacemos inner join porque buscamos una pelicula en concreto.
+SELECT `first_name`, `last_name`
+	FROM `actor`
+INNER JOIN `film_actor`
+	USING (`actor_id`)
+INNER JOIN `film`
+	USING (`film_id`)
+WHERE `title` = 'Indian Love';
+
 -- 14. Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
+SELECT `title`, `description`
+	FROM `film`
+WHERE `description` LIKE '% dog %' OR `description` LIKE '% cat %'; -- ponemos espacios para que no coja palabras que contengan dog o cat
 
 -- 15. Hay algún actor o actriz que no apareca en ninguna película en la tabla `film_actor`.
+-- No hay ningun actor que no haya actuado en alguna pelicula.
+SELECT `first_name`, `last_name`
+	FROM `actor`
+LEFT JOIN `film_actor` -- hacemos left join para coger TODOS los actores
+	USING (`actor_id`)
+WHERE `film_id` IS NULL;
+
 
 -- 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.
 
